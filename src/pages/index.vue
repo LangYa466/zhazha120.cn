9bccc73ef490b84e831926c36c4d7a1da14997e8
WOSHIZHAZHA120 <WOSHIZHAZHA120@qq.com>
2025-04-12 01:19:36 +0800

    :sparkles: 完善标题

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,9 @@
+<script lang="ts" setup>
+definePageMeta({
+	name: '首页'
+})
+</script>
+
 <template>
 	<div class="lt-md:pt-10">
 		<n-flex :size="0" vertical>
--- src/plugins/head.ts
+++ src/plugins/head.ts
@@ -2,15 +2,23 @@ import information from '~/data/information'
 
 export default defineNuxtPlugin(() => {
 	useHead({
-		titleTemplate: () => {
-			const route = useRoute()
+		title: information.name
+	})
 
-			if (route.name === undefined) {
-				return information.name
+	const router = useRouter()
+
+	router.afterEach(() => {
+		useHead({
+			titleTemplate: () => {
+				const route = useRoute()
+
+				if (route.name === undefined) {
+					return information.name
+				}
+
+				return [information.name, route.name].join(' | ')
 			}
-
-			return [information.name, route.name].join(' | ')
-		}
+		})
 	})
 
 	useFavicon(information.avatar)
